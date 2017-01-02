e34bba59607a9ea329d1bdb3010a4e33331cd724
Ben Sheffield <bshefb@gmail.com>
2017-01-02 14:45:03 +0000

    Added a count down timer to game.

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
 import cv2
 import random
+import time
 from item import Item
 
 class Game:
     def __init__(self):
+        self.real_time = 0
         self.running = False
+        self.playing = False
         self.points = 0
+        self.time = 0
         self.faces = []
+        self.items = []
 
         # Initizlize camera
         self.camera = cv2.VideoCapture()
@@ -17,43 +22,47 @@ class Game:
         # Load cascade
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-        # Initizile items
-        self.items = []
-        for _ in range(3):
-            item = Item(random.randint(0, self.width-50), random.randint(0, self.height-50))
-            self.items.append(item)
-
     # Update method
-    def update(self):
+    def update(self, dt):
         # Get the next webcam frame
         flag, self.frame = self.camera.read()
 
-        # Add items until their are 3
-        while len(self.items) < 3:
-            item = Item(random.randint(0, self.width-50), random.randint(0, self.height-50))
-            self.items.append(item)
+        if self.playing:
+            # Check if time is up
+            self.time += dt
+            if self.time >= 20:
+                self.playing = False
+                print self.points
 
-        # Detect faces in frame
-        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, 1.2, 5, minSize=(150, 150))
-        self.faces = []
-        for (x, y, width, height) in faces:
-            self.faces.append((x, y, width, height))
+            # Add items until their are 3
+            while len(self.items) < 3:
+                item = Item(random.randint(0, self.width-50), random.randint(0, self.height-50))
+                self.items.append(item)
 
-            # Check if face collides with any items
-            for i in self.items:
-                if i.isColliding(x, y, width, height):
-                    self.points += i.points
-                    self.items.remove(i)
+            # Detect faces in frame
+            gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+            faces = self.face_cascade.detectMultiScale(gray, 1.2, 5, minSize=(150, 150))
+            self.faces = []
+            for (x, y, width, height) in faces:
+                self.faces.append((x, y, width, height))
+
+                # Check if face collides with any items
+                for i in self.items:
+                    if i.isColliding(x, y, width, height):
+                        self.points += i.points
+                        self.items.remove(i)
 
         # Check for key press
         key = cv2.waitKey(33)
-        if key != -1:
-            # Exit game
+        if key == 1048603:
+            # Escape key pressed, exit game
             self.running = False
+        elif key == 1048608 and not self.playing:
+            # Space key pressed, start game
+            self.playing = True
 
     # Render method
-    def render(self):
+    def render(self, dt):
         # Render items
         for i in self.items:
             i.render(self.frame)
@@ -66,6 +75,10 @@ class Game:
         text = "Score: " + str(self.points)
         cv2.putText(self.frame, text, (20, 40), cv2.FONT_ITALIC, 1, (0, 255, 0))
 
+        # Render time
+        text = "Time: " + str(int(20 - self.time))
+        cv2.putText(self.frame, text, (20, 80), cv2.FONT_ITALIC, 1, (0, 255, 255)) 
+
         # Push frame to window
         cv2.imshow("Camera", self.frame)
 
@@ -73,8 +86,10 @@ class Game:
     def run(self):
         self.running = True
         while self.running:
-            self.update()
-            self.render()
+            current_time = time.time()
+            self.update(current_time - self.real_time)
+            self.render(current_time - self.real_time)
+            self.real_time = current_time
 
     # Clean up any objects
     def destroy(self):
