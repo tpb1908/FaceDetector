1a9a5b6e4c1a3b61d6ff630585bf5e47174d9514
Ben Sheffield <bshefb@gmail.com>
2017-01-02 12:52:21 +0000

    Converted game to its own class and seperated the update and render methods

--- game.py
+++ game.py
@@ -1,20 +1,41 @@
 import cv2
 
-# Initizlize camera
-camera = cv2.VideoCapture()
-camera.open(0)
+class Game:
+    def __init__(self):
+        self.running = False
 
-while True:
-    # Get the next webcam frame
-    flag, frame = camera.read()
-    cv2.imshow("Camera", frame)
+        # Initizlize camera
+        self.camera = cv2.VideoCapture()
+        self.camera.open(0)
 
-    # Check for key press
-    key = cv2.waitKey(33)
-    if key != -1:
-        # Exit game
-        break
+    # Update method
+    def update(self):
+        # Get the next webcam frame
+        flag, self.frame = self.camera.read()
 
-# Clean up
-cv2.destroyAllWindows()
-camera.release()
+        # Check for key press
+        key = cv2.waitKey(33)
+        if key != -1:
+            # Exit game
+            self.running = False
+
+    # Render method
+    def render(self):
+        cv2.imshow("Camera", self.frame)
+
+    # Starts the game loop
+    def run(self):
+        self.running = True
+        while self.running:
+            self.update()
+            self.render()
+
+    # Clean up any objects
+    def destroy(self):
+        # Clean up
+        cv2.destroyAllWindows()
+        self.camera.release()
+
+game = Game()
+game.run()
+game.destroy()
