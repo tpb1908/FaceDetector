20fd8d151d06fa073c95275acd9f5438779f5947
bshefb <bshefb@gmail.com>
2017-02-01 08:08:48 -0500

    Cleaned up comments and names in Face.py

--- Face.py
+++ Face.py
@@ -2,29 +2,29 @@ from skimage import transform
 from scipy.fftpack import dct
 
 class Face(object):
-
-    def __init__(self, (x, y, w, h), image):
+    RETAIN = 8
+  
+    def __init__(self, (x, y, width, height), image):
         self._x = x
         self._y = y
-        self._w = w
-        self._h = h
+        self._width = width
+        self._height = height
         
-        # This will crop the frame to the face
+        # Crop the frame to the face
         image = image[y:y + h, x:x + w] 
         image = transform.resize(image, (w, h))
 
-        # 2d-dct and truncate
+        # Extract features
         face_dct = dct(dct(image.T).T)
-        retain = 8
-        self._features = face_dct[:retain, :retain].flatten().reshape((1, -1))
+        self._features = face_dct[:RETAIN, :RETAIN].flatten().reshape((1, -1))
 
         # Find the centre of a face
     def centroid(self):
-        return self._x + int(self._w / 2), self._y + int(self._h / 2)
+        return self._x + int(self._width / 2), self._y + int(self._height / 2)
 
     def features(self):
         return self._features
 
     def shape(self):
-        return (x:self._x, y:self._y, w:self._w, h:self._h)
+        return (x:self._x, y:self._y, w:self._width, h:self._height)
 
