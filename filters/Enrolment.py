f814522dfab40b6ccf906ecd20dfc0235f7e0557
tpb1908 <theo1908phone@gmail.com>
2017-02-22 19:18:59 +0000

    Fixed syntax.

--- filters/Enrolment.py
+++ filters/Enrolment.py
@@ -1,4 +1,5 @@
-from Filters.Filter import Filter
+from filters.Filter import Filter
+
 
 class Enrolment(Filter):
 
@@ -6,29 +7,29 @@ class Enrolment(Filter):
 
     def __init__(self, width, height, sense, active=False):
         super(Enrolment, self).__init__(width, height, Enrolment.NAME, sense, active)
-        
-        self.name = None
 
-        names = open("names", "a")
-        names.write(name)
-        names.close()
-        # Collect and save images using cv2
-        retain = 8
-        w, h = 100, 100
+        self._images = []
+        self.name = None
 
     def start(self): # Start recording features
         self.name = raw_input("What is your name? ")
-        self.name = "person_{}".format(name)
+        self.name = "person_{}".format(self.name)
+
+        names = open("names", "a")
+        names.write(self.name)
+        names.close()
+
         self._images = []
-    
-    def done(self): # Complete the recording of a person
+
+    def done(self):  # Complete the recording of a person
         pass
 
     def process_frame(self, frame):
-        if self.name is None return frame # If we aren't recording
+        if self.name is None:  # If we aren't recording
+            return frame
 
         people = self._sense.live_people()
-        if len(people) > 1 or len(people) == 0: # We can only deal with one person
+        if len(people) > 1 or len(people) == 0:  # We can only deal with one person
             print "Can't enrol " + str(len(people))
         else:
             self._images.append(people[0].face().features())
