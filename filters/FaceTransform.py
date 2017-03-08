fff675f12af67fb48bc1e24ba785f12d8aa0b6bf
Theo Pearson-Bray <tpb1908dev@gmail.com>
2017-03-08 10:20:49 +0000

    Drew nose and eyes correctly.

--- filters/FaceTransform.py
+++ filters/FaceTransform.py
@@ -20,23 +20,18 @@ class FaceTransform(Filter):
 
             for _, person in people.iteritems():
                 # Get landmarks
-                landmarks = person.face().landmarks
+                face = person.face()
+                shape = face.shape()
+                landmarks = face.landmarks
                 if landmarks is None: break
-                d = person.face().dlib_rect()
-                print(person.face().shape())
-                print(" Left: {} Top: {} Right: {} Bottom: {}".format(
-                d.left(), d.top(), d.right(), d.bottom())) 
-                print(person.face().image().sum())
-                cv2.imwrite("test.jpg", person.face().image())
                 
-                aligned = FaceTransform.FACE_ALIGNER.align(person.face().shape().width, person.face().uint8_image(), dlib.rectangle(0, 0, person.face().shape().width, person.face().shape().width), landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
-                x = person.face().shape().x
-                y = person.face().shape().y
-                if((person.shape().width + x > self.width) or x < 0): break
-                if((person.shape().height + y > self.height) or y < 0): break
+                aligned = FaceTransform.FACE_ALIGNER.align(shape.width, face.uint8_image(), dlib.rectangle(0, 0, shape.width, shape.width), landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+                x = shape.x
+                y = shape.y
+                if(shape.width + x > self.width or x < 0): break
+                if(shape.height + y > self.height or y < 0): break
                 frame[y:y+aligned.shape[0], x:x+aligned.shape[1], 0] = aligned
                 frame[y:y+aligned.shape[0], x:x+aligned.shape[1], 1] = aligned
                 frame[y:y+aligned.shape[0], x:x+aligned.shape[1], 2] = aligned
-                # cv2.imwrite("test.jpg", aligned)
 
         return frame
